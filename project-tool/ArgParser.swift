4cc356229243bd386d373abf57be2418c80e015b
Simeon Leifer <simeon@droolingcat.com>
2018-07-31 05:59:54 -0700

    a bit of refactor

--- project-tool/ArgParser.swift
+++ project-tool/ArgParser.swift
@@ -361,3 +361,32 @@ class ArgParser {
         }
     }
 }
+
+extension Collection where Element == String {
+    func maxCount() -> Int {
+        var maxCount = 0
+        for item in self {
+            let count = item.count
+            if count > maxCount {
+                maxCount = count
+            }
+        }
+        return maxCount
+    }
+
+    func splittingShortArgs() -> [String] {
+        return self.map { (item) -> [String] in
+            var items: [String] = []
+            if item.hasPrefix("-") == true && item.hasPrefix("--") == false {
+                for char in item {
+                    if char != "-" {
+                        items.append("-\(char)")
+                    }
+                }
+            } else {
+                return [item]
+            }
+            return items
+            }.reduce([String](), +)
+    }
+}
